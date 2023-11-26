c97aaef8b0dd11c6caff7f1379944b8540238a2b
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-11-27 00:44:48 +0100

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -59,7 +59,7 @@ async def Cb_Handle(bot:Client, query:CallbackQuery):
         
         text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{file.file_name}`\n\n**File Size** :- `{humanize.naturalsize(file.file_size)}`"""
         buttons = [[InlineKeyboardButton("", callback_data="rename")],
-                    [InlineKeyboardButton("📝 𝖲𝖳𝖠𝖱𝖳 𝖤𝖣𝖨𝖳𝖨𝖭𝖦 📝", callback_data="compress")]]
+                    [InlineKeyboardButton("📝 𝖢𝖧𝖠𝖭𝖦𝖤 𝖬𝖤𝖳𝖠𝖣𝖠𝖳𝖠 📝", callback_data="compress")]]
         
         await query.message.edit(text=text, reply_markup=InlineKeyboardMarkup(buttons))
         
